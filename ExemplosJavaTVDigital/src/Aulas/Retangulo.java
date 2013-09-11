4d9bfde5c3fd2aff7acf7406bcf4a48f333797cc
lucianasena <luciana.aguiar.sena@gmail.com>
2013-09-11 09:14:33 -0400

    Atualização do exercício 4 do slide 3.

--- ExemplosJavaTVDigital/src/Aulas/Retangulo.java
+++ ExemplosJavaTVDigital/src/Aulas/Retangulo.java
@@ -14,9 +14,16 @@ public class Retangulo {
 		double diagonal;
 		diagonal = Math.pow(largura, 2) + Math.pow(altura, 2);
 		diagonal = Math.sqrt(diagonal);
-		
 		return diagonal;
 		
+	}
+	
+	double largura() {
+		return largura;
+	}
+	
+	double altura() {
+		return altura;
 	}
 	
 
--- ExemplosJavaTVDigital/src/Aulas/RetanguloPrincipal.java
+++ ExemplosJavaTVDigital/src/Aulas/RetanguloPrincipal.java
@@ -1,19 +1,37 @@
 package Aulas;
+import java.util.Scanner;
 
 public class RetanguloPrincipal {
+	
+	private static Scanner entrada;
 
 	public static void main(String[] args) {
 		
+		double larg, alt, largura, altura;
+		entrada = new Scanner(System.in);
+		
+		System.out.print("Digite a largura do retângulo: ");
+		larg = entrada.nextDouble();
+		System.out.print("Digite a altura do retângulo: ");
+		alt = entrada.nextDouble();
+		
 		Retangulo obRetangulo;
 		
-		double area;
-		obRetangulo = new Retangulo(); //criação do objeto
+		double area, diagonal;
+		obRetangulo = new Retangulo();
 
-		obRetangulo.largura = 5;
-		obRetangulo.altura = 4;
+		obRetangulo.largura = larg;
+		obRetangulo.altura = alt;
 
-		area = obRetangulo.largura * obRetangulo.altura;
-		System.out.println("Area do Retangulo: " + area);
+		area = obRetangulo.area();
+		diagonal = obRetangulo.diagonal();
+		largura = obRetangulo.largura();
+		altura = obRetangulo.altura();
+		
+		System.out.println("\nLargura digitada: " + largura);
+		System.out.println("Altura digitada: " + altura);
+		System.out.println("Área do Retangulo: " + area);
+		System.out.println("Diagonal do Retangulo: " + diagonal);
 	}
 
 }
